7332417bc75db6e5fbceda93b00e0076269539e4
Liam Kandel <liam.kandel@gmail.com>
2024-06-06 11:58:09 -0400

    closes #1 and implements inventory button and tempalte inventory screen.

---
+++ ShopApp.Maui/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace ShopApp.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void onInventoryClicked(object sender, EventArgs e)
+        {
+            Shell.Current.GoToAsync("//Inventory");
+        }
+    }
+
+}
---
+++ ShopApp.Maui/Views/InventoryView.xaml.cs
@@ -0,0 +1,9 @@
+namespace ShopApp.Maui.Views;
+
+public partial class InventoryView : ContentPage
+{
+	public InventoryView()
+	{
+		InitializeComponent();
+	}
+}
